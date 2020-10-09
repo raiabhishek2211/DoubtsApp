f7469bb39e761d059b1bbef2a8e7bc30b9228b00
raiabhishek2211 <rai.abhishek2211@gmail.com>
2020-10-09 12:15:34 +0530

    hello

---
+++ app/src/main/java/com/example/shopgram/teacherLogin.java
@@ -0,0 +1,20 @@
+package com.example.shopgram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class teacherLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_teacher_login);
+
+    }
+}
